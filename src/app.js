da84076cc28a110e9aa7042555b853405a46191b
Niluuu <saidumarovanilufar@mail.ru>
2019-12-16 12:09:51 +0500

    add redux, asyc data fetch

--- src/app.js
+++ src/app.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux';
 
-export default class App extends Component {
+
+
+class App extends Component {
   render() {
+    const { products, error, pending } = this.props;
+    console.log(this.props)
     return (
       <div>
-        
+        hi
       </div>
     )
   }
 }
 
+const mapStateToProps = state => ({
+  products: state.products.products,
+  pending: state.products.pending,
+  error: state.products.error
+})
+
+export default connect(mapStateToProps)(App) 
--- src/index.js
+++ src/index.js
@@ -3,8 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './app';
 import * as serviceWorker from './serviceWorker';
+import { applyMiddleware, createStore } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers/index';
+import { Provider } from 'react-redux'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const middlewares = [thunk];
+const store = createStore(
+  rootReducer,
+  applyMiddleware(thunk),
+  // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+);
+
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+  , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
